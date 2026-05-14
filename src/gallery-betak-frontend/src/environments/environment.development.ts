02c7e29d94952d9c278d3b9a84bbaff8391a8bce
0x0desha74 <mudesha74@gmail.com>
2026-05-15 00:48:04 +0300

    chore: add google oauth client ids

--- src/gallery-betak-frontend/src/environments/environment.development.ts
+++ src/gallery-betak-frontend/src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:5000/api/v1',
   paymobPublicKey: 'pk_test_placeholder',
-  googleClientId: '399998665524-r0bqphnqe11h2ld0f54sr940jgqfakmb.apps.googleusercontent.com'
+  googleClientId: '94419475994-62d4c6212acvuii856ad6h7ttd4b8d3u.apps.googleusercontent.com'
 };
--- src/gallery-betak-frontend/src/environments/environment.ts
+++ src/gallery-betak-frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: '/api/v1',
   paymobPublicKey: 'pk_live_placeholder',
-  googleClientId: '399998665524-r0bqphnqe11h2ld0f54sr940jgqfakmb.apps.googleusercontent.com'
+  googleClientId: '94419475994-62d4c6212acvuii856ad6h7ttd4b8d3u.apps.googleusercontent.com'
 };
